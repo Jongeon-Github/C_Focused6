7989275ca3b6abe799f0448997e985405030f963
eonesty <eonesty@naver.com>
2023-03-14 15:07:13 -0400

    new

---
+++ f6.cpp
@@ -0,0 +1,7 @@
+/*
+* Filename: f6.cpp
+* Project: Focused Assignment 6
+* By: Jongeon Lee
+* Date: Feb 24, 2023
+* Description: Write a program to give you practice at using pointer variables to modify parameters.
+*/
